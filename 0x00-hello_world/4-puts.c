c253e304afbc0c82ca8795d73f7341817890b2c0
pluginxdev <paulolawale@gmail.com>
2023-10-05 08:57:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main(void)
+int main(void)/* This function prints the message using puts */
 {
-  puts("Programming is like building a multilingual puzzle");
-  return (0);
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
