a43ce6212481eb7d27f8e9ab9444d78ba65c6c9f
pluginxdev <paulolawale@gmail.com>
2023-10-11 18:09:04 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,6 @@
-int putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+#pragma GCC diagnostic warning "-Wall"
+#pragma GCC diagnostic error "-Werror"
+#pragma GCC diagnostic warning "-pedantic"
+#pragma GCC diagnostic warning "-Wextra"
+#pragma GCC diagnostic warning "-std=gnu89"
+
