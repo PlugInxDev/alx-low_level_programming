2370f5158f93bb9b72f3185cd93fdd8aa2dba1f4
pluginxdev <paulolawale@gmail.com>
2023-10-11 18:20:19 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - function that print
+ * alphabet 
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+}
+_putchar (letter);
+{
+	_putchar(10);
+}
