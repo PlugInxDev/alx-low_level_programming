525a7baf72ac636a3763ee94694967ad177287cb
pluginxdev <paulolawale@gmail.com>
2023-11-01 08:56:13 -0700

    What is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int__attribute__((_unused_)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
