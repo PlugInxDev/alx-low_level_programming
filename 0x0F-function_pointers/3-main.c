8fcde8db6e777da8d020fca5e0dcec1afa33c12b
pluginxdev <paulolawale@gmail.com>
2023-11-08 14:33:24 -0800

    A program taht performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,14 +1,13 @@
-nclude "3-calc.h"
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * * main - Prints the result of simple operations.
- * * @argc: The number of arguments supplied to the program.
- * * @argv: An array of pointers to the arguments.
- * *
- * * Return: Always 0.
- * */
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0.
+ */
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
