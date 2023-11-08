f8ee4560474b5ad89576f1f78dba4a79008338af
pluginxdev <paulolawale@gmail.com>
2023-11-08 14:08:35 -0800

    A function that searches for a integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,29 +1,23 @@
-#include "function_pointer.h"
+#include "function_pointers.h"
 
-/**int_index - searches for an integer
- * @array: Array containing elements
- * @size: number of elements in array
- * @cmp: function which compare values
- *
- * Return: returns -1 if no element match
- * or when size is less than zero
- * return pointer to the first corresponding element
- *
+/**
+ *  int_index - ...
+ *  @array: ...
+ *  @size: ...
+ *  @cmp: ...
+ *  Return: ...
  */
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int index = 0;
-
-	if (array == NULL || cmp == NULL)
-		return (-1);
-
-	for (; index < size; index++)
+	int i = 0;
+	if (size > 0)
 	{
-		if (cmp(array[index]) != 0)
-			return (index);
+		if (array != NULL && cmp != NULL)
+		{													while (i < size)
+			{													if (cmp(array[i]))
+				return (i);																								i++;
+			}
+		}
 	}
-
 	return (-1);
-
 }
